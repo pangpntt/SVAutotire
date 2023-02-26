80a584e1bce884689ad6d8c37ba8208c5438e233
PhannitaSaisorn <pang.pntt@gmail.com>
2023-02-26 13:26:45 +0700

    edit

---
+++ scrips.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+
+//     $(".toggler").on("click", function(){
+//         $('.menu-container').toggleClass('active');
+//     })
+//     function setMenuHeight(){
+//         var navbarHeight = $('.navbar').outerHeight();
+//         $('.menu-wrapper')
+//         .outerHeight(document.documentElement.clientHeight - navbarHeight).niceScroll({
+//             emulatetouch: true
+//         });
+//     }
+//     setMenuHeight();
+//     $(window).on('resize', function(){
+//         setMenuHeight();
+//     })
+// }
+// )
