8af074a74a606518b03324333f7a79e27ec59867
ArchanPhalanuphat <archan.phalanuphat@gmail.com>
2023-03-02 17:32:14 +0700

    update

--- data/sale_history.js
+++ data/sale_history.js
@@ -1,6 +1,6 @@
 const sale_history = JSON.parse(`[{
     "SALE_HISTORY_ID": 1,
-    "SALE_HISTORY_DATE": "24-2-2023",
+    "SALE_HISTORY_DATE": "2023-02-02",
     "SALE_HISTORY_PRICE": "125000",
     "SALE_HISTORY_AMOUNT": 4,
     "EMPLOYEE_NAME": "Lissy" ,
@@ -12,7 +12,7 @@ const sale_history = JSON.parse(`[{
 
 },{
     "SALE_HISTORY_ID": 2,
-    "SALE_HISTORY_DATE": "05-05-2015",
+    "SALE_HISTORY_DATE": "2023-02-02",
     "SALE_HISTORY_PRICE": "555555",
     "SALE_HISTORY_AMOUNT": 5,
     "EMPLOYEE_NAME": "Mac" ,
